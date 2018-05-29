d2504f9b10a8bb19db06b6c116d7376d172b1d8b
Andita Kaesar <anditakaesar@gmail.com>
2018-05-29 13:32:29 +0700

    Create basic side nav

--- angular-src/src/app/custommaterialmodule.module.ts
+++ angular-src/src/app/custommaterialmodule.module.ts
@@ -1,9 +1,32 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [ MatButtonModule, MatToolbarModule ],
-  exports: [ MatButtonModule, MatToolbarModule ]
+  imports: [ 
+    BrowserAnimationsModule,    
+    MatButtonModule, 
+    MatToolbarModule, 
+    MatMenuModule, 
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
+  ],
+  exports: [ 
+    MatButtonModule, 
+    MatToolbarModule, 
+    MatMenuModule, 
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule
+  ]
 })
 export class CustommaterialmoduleModule { }
