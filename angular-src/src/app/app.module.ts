14f1eff058b09d1eca95e8ab526f9580a72e9cd1
Andita Kaesar <anditakaesar@gmail.com>
2018-05-29 11:28:09 +0700

    Create basic components
    Login
    Register
    Dashboard
    Profile

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
