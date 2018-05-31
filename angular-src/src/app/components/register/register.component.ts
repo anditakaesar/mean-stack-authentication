2bd89318ee7798e78683164236b5160929573830
Andita Kaesar <anditakaesar@gmail.com>
2018-05-31 08:43:40 +0700

    Add basic forms for registration
    with action buttons

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  hide: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  btnClickNavigate(links: Array<string>) {
+    this.router.navigate(links);
+  }
 }
